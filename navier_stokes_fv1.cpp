2c716d5302a360d7e4fb1bfc875f6ff52aaf0fa1
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2012-03-06 09:55:03 +0000

    renaming 'get_function_pattern' -> 'function_pattern' and 'get_element_corners' -> 'element_corners'

--- navier_stokes_fv1.cpp
+++ navier_stokes_fv1.cpp
@@ -138,7 +138,7 @@ FVNavierStokesElemDisc<TDomain>::
 prepare_element(TElem* elem, const LocalVector& u)
 {
 //	get corners
-	m_vCornerCoords = this->template get_element_corners<TElem>(elem);
+	m_vCornerCoords = this->template element_corners<TElem>(elem);
 
 // 	Update Geometry for this element
 	TFVGeom<TElem, dim>& geo = Provider<TFVGeom<TElem,dim> >::get();
