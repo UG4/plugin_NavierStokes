d186a976de76b5e6ed2d25b710fca1b5ebc28df7
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2012-04-10 11:12:26 +0000

    Working on IElemDisc: adding second possibility for assembling functions, a virtual interface. The currently assembling functions by explicitly setting a function pointer are still available and recommended. The can be used after calling 'set_fast_ass_elem' prior to registering the functions. NOTE: Work on second possibility not yet finish and thus not usable, will be continued soon.

--- navier_stokes_fv1.cpp
+++ navier_stokes_fv1.cpp
@@ -808,6 +808,7 @@ register_fv1_func()
 	ReferenceObjectID id = geometry_traits<TElem>::REFERENCE_OBJECT_ID;
 	typedef this_type T;
 
+	this->enable_fast_ass_elem(true);
 	set_prep_elem_loop_fct(	id, &T::template prepare_element_loop<TElem, TFVGeom>);
 	set_prep_elem_fct(	 	id, &T::template prepare_element<TElem, TFVGeom>);
 	set_fsh_elem_loop_fct( 	id, &T::template finish_element_loop<TElem, TFVGeom>);
