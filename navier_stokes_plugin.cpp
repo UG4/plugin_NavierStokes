c06eb0208aa1e52a204b17401c5e173e1f996ad4
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2012-05-24 16:19:44 +0000

    Adapting NavierStokes plugin to new registering style.

--- navier_stokes_plugin.cpp
+++ navier_stokes_plugin.cpp
@@ -1,13 +1,12 @@
-// created by Sebastian Reiter
-// s.b.reiter@googlemail.com
-// 14.09.2011 (m,d,y)
+/**
+ * File for registration of navier stokes routines.
+ *
+ * created by Sebastian Reiter
+ * s.b.reiter@googlemail.com
+ * 14.09.2011 (m,d,y)
+ */
 
-// extern headers
-#include <iostream>
-#include <sstream>
-#include <string>
-
-#include "bridge/bridge.h"
+#include "bridge/util.h"
 
 #include "navier_stokes.h"
 #include "upwind.h"
@@ -15,36 +14,40 @@
 #include "navier_stokes_bnd.h"
 #include "no_normal_stress_outflow.h"
 
-#include "lib_algebra/cpu_algebra_types.h"
-
 using namespace std;
 using namespace ug::bridge;
 
 namespace ug{
 namespace NavierStokes{
 
-template <typename TDomain, typename TAlgebra>
-static void Register__Algebra_Domain(Registry& reg, string parentGroup)
+/**
+ * Class exporting the functionality of the plugin. All functionality that is to
+ * be used in scripts or visualization must be registered here.
+ */
+struct Functionality
 {
-//	typedef
+
+/**
+ * Function called for the registration of Domain and Algebra dependent parts
+ * of the plugin. All Functions and Classes depending on both Domain and Algebra
+ * are to be placed here when registering. The method is called for all
+ * available Domain and Algebra types, based on the current build options.
+ *
+ * @param reg				registry
+ * @param parentGroup		group for sorting of functionality
+ */
+template <typename TDomain, typename TAlgebra>
+static void DomainAlgebra(Registry& reg, string grp)
+{
 	static const int dim = TDomain::dim;
-
-//	group string
-	stringstream grpSS; grpSS << parentGroup << "/" << dim << "d";
-	string grp = grpSS.str();
-
-//	suffix and tag
-	string dimAlgSuffix = bridge::GetDomainSuffix<TDomain>();
-	dimAlgSuffix.append(GetAlgebraSuffix<TAlgebra>());
-
-	string dimAlgTag = GetDomainTag<TDomain>();
-	dimAlgTag.append(GetAlgebraTag<TAlgebra>());
+	string suffix = GetDomainAlgebraSuffix<TDomain,TAlgebra>();
+	string tag = GetDomainAlgebraTag<TDomain,TAlgebra>();
 
 //	NavierStokesInflow
 	{
 		typedef NavierStokesInflow<TDomain, TAlgebra> T;
 		typedef IDiscretizationItem<TDomain, TAlgebra> TBase;
-		string name = string("NavierStokesInflow").append(dimAlgSuffix);
+		string name = string("NavierStokesInflow").append(suffix);
 		reg.add_class_<T, TBase>(name, grp)
 			.template add_constructor<void (*)(const char*,const char*)>("Function(s)#Subset(s)")
 
@@ -57,39 +60,43 @@ static void Register__Algebra_Domain(Registry& reg, string parentGroup)
 #endif
 
 			.set_construct_as_smart_pointer(true);
-		reg.add_class_to_group(name, "NavierStokesInflow", dimAlgTag);
+		reg.add_class_to_group(name, "NavierStokesInflow", tag);
 	}
 
 //	NavierStokesWall
 	{
 		typedef NavierStokesWall<TDomain, TAlgebra> T;
 		typedef IDiscretizationItem<TDomain, TAlgebra> TBase;
-		string name = string("NavierStokesWall").append(dimAlgSuffix);
+		string name = string("NavierStokesWall").append(suffix);
 		reg.add_class_<T, TBase>(name, grp)
 			.template add_constructor<void (*)(const char*)>("Function(s)")
 			.add_method("add", &T::add)
 			.set_construct_as_smart_pointer(true);
-		reg.add_class_to_group(name, "NavierStokesWall", dimAlgTag);
+		reg.add_class_to_group(name, "NavierStokesWall", tag);
 	}
-
 }
 
+/**
+ * Function called for the registration of Domain dependent parts
+ * of the plugin. All Functions and Classes depending on the Domain
+ * are to be placed here when registering. The method is called for all
+ * available Domain types, based on the current build options.
+ *
+ * @param reg				registry
+ * @param parentGroup		group for sorting of functionality
+ */
 template <typename TDomain>
-static void Register__Domain(Registry& reg, string grp)
+static void Domain(Registry& reg, string grp)
 {
-
-//	dimension of domain
 	static const int dim = TDomain::dim;
-
-//	suffix and tag
-	string dimSuffix = GetDomainSuffix<TDomain>();
-	string dimTag = GetDomainTag<TDomain>();
+	string suffix = GetDomainSuffix<TDomain>();
+	string tag = GetDomainTag<TDomain>();
 
 //	Navier-Stokes
 	{
 		typedef NavierStokes<TDomain> T;
 		typedef IDomainElemDisc<TDomain> TBase;
-		string name = string("NavierStokes").append(dimSuffix);
+		string name = string("NavierStokes").append(suffix);
 		reg.add_class_<T, TBase >(name, grp)
 			.template add_constructor<void (*)(const char*,const char*)>("Functions#Subset(s)")
 
@@ -115,98 +122,113 @@ static void Register__Domain(Registry& reg, string grp)
 			.add_method("set_laplace", &T::set_laplace)
 			.add_method("set_stokes", &T::set_stokes)
 			.set_construct_as_smart_pointer(true);
-		reg.add_class_to_group(name, "NavierStokes", dimTag);
+		reg.add_class_to_group(name, "NavierStokes", tag);
 	}
 
 //	FVNavierStokesNoNormalStressOutflow
 	{
 		typedef FVNavierStokesNoNormalStressOutflow<TDomain> T;
 		typedef IDomainElemDisc<TDomain> TBase;
-		string name = string("FVNavierStokesNoNormalStressOutflow").append(dimSuffix);
+		string name = string("FVNavierStokesNoNormalStressOutflow").append(suffix);
 		reg.add_class_<T, TBase>(name, grp)
 			.template add_constructor<void (*)(SmartPtr< NavierStokes<TDomain> >)>("MasterDisc")
 			.add_method("add", &T::add, "", "Subset(s)")
 			.set_construct_as_smart_pointer(true);
-		reg.add_class_to_group(name, "FVNavierStokesNoNormalStressOutflow", dimTag);
+		reg.add_class_to_group(name, "FVNavierStokesNoNormalStressOutflow", tag);
 	}
+}
+
+/**
+ * Function called for the registration of Dimension dependent parts
+ * of the plugin. All Functions and Classes depending on the Dimension
+ * are to be placed here when registering. The method is called for all
+ * available Dimension types, based on the current build options.
+ *
+ * @param reg				registry
+ * @param parentGroup		group for sorting of functionality
+ */
+template <int dim>
+static void Dimension(Registry& reg, string grp)
+{
+	string suffix = GetDimensionSuffix<dim>();
+	string tag = GetDimensionTag<dim>();
 
 /////////////////////////////////////////////////////////////////////////////
 // Upwind
 /////////////////////////////////////////////////////////////////////////////
 
-
 //	INavierStokesUpwind
 	{
 		typedef INavierStokesUpwind<dim> T;
-		string name = string("INavierStokesUpwind").append(dimSuffix);
+		string name = string("INavierStokesUpwind").append(suffix);
 		reg.add_class_<T>(name, grp);
-		reg.add_class_to_group(name, "INavierStokesUpwind", dimTag);
+		reg.add_class_to_group(name, "INavierStokesUpwind", tag);
 	}
 
 //	NavierStokesNoUpwind
 	{
 		typedef NavierStokesNoUpwind<dim> T;
 		typedef INavierStokesUpwind<dim> TBase;
-		string name = string("NavierStokesNoUpwind").append(dimSuffix);
+		string name = string("NavierStokesNoUpwind").append(suffix);
 		reg.add_class_<T, TBase>(name, grp)
 			.add_constructor()
 			.set_construct_as_smart_pointer(true);
-		reg.add_class_to_group(name, "NavierStokesNoUpwind", dimTag);
+		reg.add_class_to_group(name, "NavierStokesNoUpwind", tag);
 	}
 
 //	NavierStokesFullUpwind
 	{
 		typedef NavierStokesFullUpwind<dim> T;
 		typedef INavierStokesUpwind<dim> TBase;
-		string name = string("NavierStokesFullUpwind").append(dimSuffix);
+		string name = string("NavierStokesFullUpwind").append(suffix);
 		reg.add_class_<T, TBase>(name, grp)
 			.add_constructor()
 			.set_construct_as_smart_pointer(true);
-		reg.add_class_to_group(name, "NavierStokesFullUpwind", dimTag);
+		reg.add_class_to_group(name, "NavierStokesFullUpwind", tag);
 	}
 
 //	NavierStokesSkewedUpwind
 	{
 		typedef NavierStokesSkewedUpwind<dim> T;
 		typedef INavierStokesUpwind<dim> TBase;
-		string name = string("NavierStokesSkewedUpwind").append(dimSuffix);
+		string name = string("NavierStokesSkewedUpwind").append(suffix);
 		reg.add_class_<T, TBase>(name, grp)
 			.add_constructor()
 			.set_construct_as_smart_pointer(true);
-		reg.add_class_to_group(name, "NavierStokesSkewedUpwind", dimTag);
+		reg.add_class_to_group(name, "NavierStokesSkewedUpwind", tag);
 	}
 
 //	NavierStokesLinearProfileSkewedUpwind
 	{
 		typedef NavierStokesLinearProfileSkewedUpwind<dim> T;
 		typedef INavierStokesUpwind<dim> TBase;
-		string name = string("NavierStokesLinearProfileSkewedUpwind").append(dimSuffix);
+		string name = string("NavierStokesLinearProfileSkewedUpwind").append(suffix);
 		reg.add_class_<T, TBase>(name, grp)
 			.add_constructor()
 			.set_construct_as_smart_pointer(true);
-		reg.add_class_to_group(name, "NavierStokesLinearProfileSkewedUpwind", dimTag);
+		reg.add_class_to_group(name, "NavierStokesLinearProfileSkewedUpwind", tag);
 	}
 
 //	NavierStokesPositiveUpwind
 	{
 		typedef NavierStokesPositiveUpwind<dim> T;
 		typedef INavierStokesUpwind<dim> TBase;
-		string name = string("NavierStokesPositiveUpwind").append(dimSuffix);
+		string name = string("NavierStokesPositiveUpwind").append(suffix);
 		reg.add_class_<T, TBase>(name, grp)
 			.add_constructor()
 			.set_construct_as_smart_pointer(true);
-		reg.add_class_to_group(name, "NavierStokesPositiveUpwind", dimTag);
+		reg.add_class_to_group(name, "NavierStokesPositiveUpwind", tag);
 	}
 
 //	NavierStokesRegularUpwind
 	{
 		typedef NavierStokesRegularUpwind<dim> T;
 		typedef INavierStokesUpwind<dim> TBase;
-		string name = string("NavierStokesRegularUpwind").append(dimSuffix);
+		string name = string("NavierStokesRegularUpwind").append(suffix);
 		reg.add_class_<T, TBase>(name, grp)
 			.add_constructor()
 			.set_construct_as_smart_pointer(true);
-		reg.add_class_to_group(name, "NavierStokesRegularUpwind", dimTag);
+		reg.add_class_to_group(name, "NavierStokesRegularUpwind", tag);
 	}
 
 /////////////////////////////////////////////////////////////////////////////
@@ -217,131 +239,56 @@ static void Register__Domain(Registry& reg, string grp)
 //	INavierStokesStabilization
 	{
 		typedef INavierStokesStabilization<dim> T;
-		string name = string("INavierStokesStabilization").append(dimSuffix);
+		string name = string("INavierStokesStabilization").append(suffix);
 		reg.add_class_<T>(name, grp)
 			.add_method("set_upwind", &T::set_upwind)
 			.add_method("set_diffusion_length", &T::set_diffusion_length);
-		reg.add_class_to_group(name, "INavierStokesStabilization", dimTag);
+		reg.add_class_to_group(name, "INavierStokesStabilization", tag);
 	}
 
 //	NavierStokesFIELDSStabilization
 	{
 		typedef NavierStokesFIELDSStabilization<dim> T;
 		typedef INavierStokesStabilization<dim> TBase;
-		string name = string("NavierStokesFIELDSStabilization").append(dimSuffix);
+		string name = string("NavierStokesFIELDSStabilization").append(suffix);
 		reg.add_class_<T, TBase>(name, grp)
 			.add_constructor()
 			.set_construct_as_smart_pointer(true);
-		reg.add_class_to_group(name, "NavierStokesFIELDSStabilization", dimTag);
+		reg.add_class_to_group(name, "NavierStokesFIELDSStabilization", tag);
 	}
 
 //	NavierStokesFLOWStabilization
 	{
 		typedef NavierStokesFLOWStabilization<dim> T;
 		typedef INavierStokesStabilization<dim> TBase;
-		string name = string("NavierStokesFLOWStabilization").append(dimSuffix);
+		string name = string("NavierStokesFLOWStabilization").append(suffix);
 		reg.add_class_<T, TBase>(name, grp)
 			.add_constructor()
 			.set_construct_as_smart_pointer(true);
-		reg.add_class_to_group(name, "NavierStokesFLOWStabilization", dimTag);
+		reg.add_class_to_group(name, "NavierStokesFLOWStabilization", tag);
 	}
 
 }
 
+}; // end Functionality
+} // end namespace NavierStokes
 
 
-template <typename TAlgebra>
-static bool Register__Algebra(Registry& reg, string parentGroup)
-{
-//	get group string
-	string grp = parentGroup; grp.append("/Discretization");
-
-	try
-	{
-
-#ifdef UG_DIM_1
-//	Domain dependent part 1D
-	{
-		typedef Domain<1, MultiGrid, MGSubsetHandler> domain_type;
-		Register__Algebra_Domain<domain_type, TAlgebra>(reg, grp);
-	}
-#endif
-
-#ifdef UG_DIM_2
-//	Domain dependent part 2D
-	{
-		typedef Domain<2, MultiGrid, MGSubsetHandler> domain_type;
-		Register__Algebra_Domain<domain_type, TAlgebra>(reg, grp);
-	}
-#endif
-
-#ifdef UG_DIM_3
-//	Domain dependent part 3D
-	{
-		typedef Domain<3, MultiGrid, MGSubsetHandler> domain_type;
-		Register__Algebra_Domain<domain_type, TAlgebra>(reg, grp);
-	}
-#endif
-
-	}
-	catch(UG_REGISTRY_ERROR_RegistrationFailed ex)
-	{
-		UG_LOG("### ERROR in Register__Algebra_DoFDistribution: "
-				"Registration failed (using name " << ex.name << ").\n");
-		return false;
-	}
-
-	return true;
-}
-
-void Register(Registry* reg, string parentGroup)
-{
-	string grp(parentGroup); grp.append("NavierStokes/");
-
-	try
-	{
-#ifdef UG_CPU_1
-	Register__Algebra<CPUAlgebra>(*reg, grp);
-#endif
-#ifdef UG_CPU_2
-	Register__Algebra<CPUBlockAlgebra<2> >(*reg, grp);
-#endif
-#ifdef UG_CPU_3
-	Register__Algebra<CPUBlockAlgebra<3> >(*reg, grp);
-#endif
-#ifdef UG_CPU_4
-	Register__Algebra<CPUBlockAlgebra<4> >(*reg, grp);
-#endif
-#ifdef UG_CPU_VAR
-	Register__Algebra<CPUVariableBlockAlgebra >(*reg, grp);
-#endif
-
-#ifdef UG_DIM_1
-			Register__Domain<Domain1d>(*reg, grp);
-#endif
-#ifdef UG_DIM_2
-			Register__Domain<Domain2d>(*reg, grp);
-#endif
-#ifdef UG_DIM_3
-			Register__Domain<Domain3d>(*reg, grp);
-#endif
-	}
-	catch(UG_REGISTRY_ERROR_RegistrationFailed ex)
-	{
-		UG_LOG("### ERROR in NavierStokes plugin: "
-				"Registration failed (using name " << ex.name << ").\n");
-		return;
-	}
-
-	return;
-}
-
-} // namespace NavierStokes
-
+/**
+ * This function is called when the plugin is loaded.
+ */
 extern "C" void
-InitUGPlugin_NavierStokes(Registry* reg, string parentGroup)
+InitUGPlugin_NavierStokes(Registry* reg, string grp)
 {
-	NavierStokes::Register(reg, parentGroup);
+	grp.append("SpatialDisc/NavierStokes/");
+	typedef NavierStokes::Functionality Functionality;
+
+	try{
+		RegisterDimensionDependent<Functionality>(*reg,grp);
+		RegisterDomainDependent<Functionality>(*reg,grp);
+		RegisterDomainAlgebraDependent<Functionality>(*reg,grp);
+	}
+	UG_REGISTRY_CATCH_THROW(grp);
 }
 
-}// end of namespace
+}// namespace ug
