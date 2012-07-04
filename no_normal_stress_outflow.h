11d2bce9155894ecfa67f5c2949991eb9a69956e
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2012-07-04 12:23:44 +0000

    Splitting DataImport and DataExport into separate Files.

--- no_normal_stress_outflow.h
+++ no_normal_stress_outflow.h
@@ -13,7 +13,8 @@
 
 // library intern headers
 #include "lib_disc/spatial_disc/elem_disc/elem_disc_interface.h"
-#include "lib_disc/spatial_disc/ip_data/data_import_export.h"
+#include "lib_disc/spatial_disc/ip_data/data_export.h"
+#include "lib_disc/spatial_disc/ip_data/data_import.h"
 
 #include "upwind.h"
 #include "stabilization.h"
