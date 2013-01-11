e8c041e01d27b091442be0089ddf079ebd207740
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2013-01-11 12:10:37 +0000

    removing unused includes.

--- navier_stokes_cr.h
+++ navier_stokes_cr.h
@@ -9,10 +9,6 @@
 
 #include "common/util/provider.h"
 #include "lib_disc/spatial_disc/disc_util/cr_finite_volume_geometry.h"
-#include "lib_disc/spatial_disc/user_data/const_user_data.h"
-#ifdef UG_FOR_LUA
-#include "bindings/lua/lua_user_data.h"
-#endif
 
 namespace ug{
 namespace NavierStokes{
