7d78e585c6b2caedb55f64c99933dac0303adbf5
Christian Wehner <christian.wehner@gcsc.uni-frankfurt.de>
2013-03-01 16:34:31 +0000

    small changes

--- turbulent_viscosity_data_impl.h
+++ turbulent_viscosity_data_impl.h
@@ -8,17 +8,7 @@
 #ifndef __H__UG__NAVIER_STOKES_TURBULENT_VISCOSITY_DATA_IMPL_H_
 #define __H__UG__NAVIER_STOKES_TURBULENT_VISCOSITY_DATA_IMPL_H_
 
-#include "common/common.h"
-
 #include "turbulent_viscosity_data.h"
-#include "lib_disc/common/subset_group.h"
-#include "lib_disc/common/function_group.h"
-#include "lib_disc/common/groups_util.h"
-#include "lib_disc/quadrature/quadrature.h"
-#include "lib_disc/local_finite_element/local_shape_function_set.h"
-#include "lib_disc/spatial_disc/user_data/user_data.h"
-#include "lib_disc/reference_element/reference_mapping_provider.h"
-#include "lib_grid/tools/periodic_boundary_manager.h"
 
 namespace ug{
 namespace NavierStokes{
