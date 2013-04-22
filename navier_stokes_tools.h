945e6e25c785e07f3e92e87e86a513a24144d7b4
Christian Wehner <christian.wehner@gcsc.uni-frankfurt.de>
2013-04-22 16:54:38 +0000

    vorticity computation for Lagrange1 functions

--- navier_stokes_tools.h
+++ navier_stokes_tools.h
@@ -19,7 +19,7 @@ namespace ug{
 // compute vorticity
 // for velocity field (u,v,w) the vorticity is \partial_x v - \partial_y u
 template <typename TGridFunction>
-void vorticity(TGridFunction& vort,TGridFunction& u)
+void vorticityFVCR(TGridFunction& vort,TGridFunction& u)
 {
 	///	domain type
 	typedef typename TGridFunction::domain_type domain_type;
@@ -164,6 +164,155 @@ void vorticity(TGridFunction& vort,TGridFunction& u)
 		}
 	}
 	grid.template detach_from<side_type>(m_aVolume);
+}
+
+template <typename TGridFunction>
+void vorticityFV1(TGridFunction& vort,TGridFunction& u)
+{
+	///	domain type
+	typedef typename TGridFunction::domain_type domain_type;
+
+	///	grid type
+	typedef typename domain_type::grid_type grid_type;
+
+	///	world dimension
+	static const int dim = domain_type::dim;
+
+	// get grid
+	grid_type& grid = *u.domain()->grid();
+
+	// position accessor type
+	typedef typename domain_type::position_accessor_type position_accessor_type;
+
+	/// element type
+	typedef typename TGridFunction::template dim_traits<dim>::geometric_base_object elem_type;
+
+	//  volume attachment
+	typedef PeriodicAttachmentAccessor<VertexBase,ANumber > aSideNumber;
+	aSideNumber m_acVolume;
+	ANumber m_aVolume;
+
+	/// element iterator
+	typedef typename TGridFunction::template dim_traits<dim>::const_iterator ElemIterator;
+
+	/// side iterator
+	typedef typename TGridFunction::template traits<VertexBase>::const_iterator VertexBaseConstIterator;
+
+	//	get position accessor
+	typedef typename domain_type::position_accessor_type position_accessor_type;
+	position_accessor_type& posAcc = u.domain()->position_accessor();
+
+	DimFV1Geometry<dim> geo;
+
+	grid.template attach_to<VertexBase>(m_aVolume);
+	m_acVolume.access(grid,m_aVolume);
+
+	SetAttachmentValues(m_acVolume,grid.template begin<VertexBase>(), grid.template end<VertexBase>(), 0);
+
+	if (vort.local_finite_element_id(0) != LFEID(LFEID::LAGRANGE, 1)){
+				UG_THROW("Component " << 0 << " in approximation space of parameter 1 must be of Crouzeix-Raviart type.");
+	}
+
+	for (int d=0;d<dim;d++){
+		if (u.local_finite_element_id(d) != LFEID(LFEID::LAGRANGE, 1)){
+			UG_THROW("Component " << d << " in approximation space of parameter 2 must be of Crouzeix-Raviart type.");
+		}
+	}
+
+	PeriodicBoundaryManager* pbm = grid.periodic_boundary_manager();
+
+	//	coord and vertex array
+	MathVector<dim> coCoord[domain_traits<dim>::MaxNumVerticesOfElem];
+	VertexBase* vVrt[domain_traits<dim>::MaxNumVerticesOfElem];
+
+	//	create Multiindex
+	std::vector<MultiIndex<2> > multInd;
+
+	for(int si = 0; si < u.num_subsets(); ++si)
+	{
+		if (si>0) continue;
+		//	get iterators
+		ElemIterator iter = u.template begin<elem_type>(si);
+		ElemIterator iterEnd = u.template end<elem_type>(si);
+
+		//	loop elements of dimension
+		for(  ;iter !=iterEnd; ++iter)
+		{
+			//	get Elem
+			elem_type* elem = *iter;
+			//	get vertices and extract corner coordinates
+			const size_t numVertices = elem->num_vertices();
+			for(size_t i = 0; i < numVertices; ++i){
+				vVrt[i] = elem->vertex(i);
+				coCoord[i] = posAcc[vVrt[i]];
+				// UG_LOG("co_coord(" << i<< "+1,:)=" << coCoord[i] << "\n");
+			}
+			//	evaluate finite volume geometry
+			geo.update(elem, &(coCoord[0]), u.domain()->subset_handler().get());
+
+			static const size_t MaxNumSidesOfElem = 18;
+
+			typedef MathVector<dim> MVD;
+			std::vector<MVD> uValue(MaxNumSidesOfElem);
+
+			for (size_t co=0;co < numVertices;co++)
+			{
+				for (int d=0;d<dim;d++){
+					//	get indices of function fct on vertex
+					u.multi_indices(elem->vertex(co), d, multInd);
+					//	read value of index from vector
+					uValue[co][d]=DoFRef(u,multInd[0]);
+				}
+			}
+
+			for (size_t co=0;co < numVertices;co++)
+			{
+				number localvort = 0;
+				const typename DimFV1Geometry<dim>::SCV& scv = geo.scv(co);
+
+				for(size_t sh = 0 ; sh < scv.num_sh(); ++sh){
+					localvort += uValue[sh][1] * scv.global_grad(sh)[0] - uValue[sh][0] * scv.global_grad(sh)[1];
+				};
+
+				number vol = scv.volume();
+
+				localvort*=vol;
+
+				vort.multi_indices(elem->vertex(co), 0, multInd);
+				DoFRef(vort,multInd[0])+=localvort;
+				m_acVolume[elem->vertex(co)] += vol;
+			}
+		}
+		// average vorticity
+		VertexBaseConstIterator vertexIter = vort.template begin<VertexBase>(si);
+		VertexBaseConstIterator vertexIterEnd = vort.template end<VertexBase>(si);
+		number maxvort = 0;
+		for(  ;vertexIter !=vertexIterEnd; vertexIter++)
+		{
+			//	get Elem
+			VertexBase* vrt = *vertexIter;
+			// if periodic slave continue
+			if (pbm && pbm->is_slave(vrt)) continue;
+			vort.multi_indices(vrt, 0, multInd);
+			DoFRef(vort,multInd[0])/=m_acVolume[vrt];
+			if (DoFRef(vort,multInd[0])*DoFRef(vort,multInd[0])>maxvort*maxvort){
+				maxvort = DoFRef(vort,multInd[0]);
+			}
+		}
+	}
+	grid.template detach_from<VertexBase>(m_aVolume);
+}
+
+template <typename TGridFunction>
+void vorticity(TGridFunction& vort,TGridFunction& u){
+	if (vort.local_finite_element_id(0) == LFEID(LFEID::LAGRANGE, 1)){
+		vorticityFV1<TGridFunction>(vort,u);
+	} else
+		if (vort.local_finite_element_id(0) == LFEID(LFEID::CROUZEIX_RAVIART, 1)){
+			vorticityFVCR<TGridFunction>(vort,u);
+		} else {
+			UG_LOG("Function type " << vort.local_finite_element_id(0) << " not supported in vorticity computation.");
+		}
 }
 
 // array0 = array1
