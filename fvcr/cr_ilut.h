f33569e20b629e035b2ae352a767086ee6e3c98b
Martin Rupp <martin.rupp@gcsc.uni-frankfurt.de>
2013-04-23 09:41:39 +0000

    CHANGES TO OPERATOR INTERFACES ! cleaned up the algebra operator interfaces. now every interface is in an own file, in the directory lib_algebra/operator/interface/ and there: ILinearIterator -> linear_iterator.h ILinearOperatorInverse -> linear_operator_inverse.h ILinearOperator -> linear_operator.h IMatrixOperatorInverse -> matrix_operator_inverse.h MatrixOperator -> matrix_operator.h IOperatorInverse -> operator_inverse.h IOperator -> operator.h IPreconditionedLinearOperatorInverse -> preconditioned_linear_operator_inverse.h IPreconditioner -> preconditioner.h
    I made all modifications in files and plugins (amg, navier_storkes and neuron) that where checked in and I could compile. Feel free to ask me if you have questions/problems with this.

--- fvcr/cr_ilut.h
+++ fvcr/cr_ilut.h
@@ -14,7 +14,7 @@
 #define __H__UG__LIB_DISC__OPERATOR__LINEAR_OPERATOR__CRILUT__
 
 #include "lib_algebra/operator/interface/operator.h"
-#include "lib_algebra/operator/interface/operator_iterator.h"
+#include "lib_algebra/operator/interface/preconditioner.h"
 #ifdef UG_PARALLEL
 	#include "lib_algebra/parallelization/parallelization.h"
 #endif
