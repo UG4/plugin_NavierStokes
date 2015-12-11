9e71d443e9dbd4d0557182c5052fc082ceba93b7
Jonas Simon <jonas.simon@gmx.net>
2015-12-11 10:41:51 +0100

    added functions for getting and setting bingham_viscosity and yield_stress

--- incompressible/fe/navier_stokes_fe.h
+++ incompressible/fe/navier_stokes_fe.h
@@ -1,33 +1,8 @@
 /*
- * Copyright (c) 2013-2014:  G-CSC, Goethe University Frankfurt
- * Author: Andreas Vogel
- * 
- * This file is part of UG4.
- * 
- * UG4 is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License version 3 (as published by the
- * Free Software Foundation) with the following additional attribution
- * requirements (according to LGPL/GPL v3 §7):
- * 
- * (1) The following notice must be displayed in the Appropriate Legal Notices
- * of covered and combined works: "Based on UG4 (www.ug4.org/license)".
- * 
- * (2) The following notice must be displayed at a prominent place in the
- * terminal output of covered works: "Based on UG4 (www.ug4.org/license)".
- * 
- * (3) The following bibliography is recommended for citation and must be
- * preserved in all covered files:
- * "Reiter, S., Vogel, A., Heppner, I., Rupp, M., and Wittum, G. A massively
- *   parallel geometric multigrid solver on hierarchically distributed grids.
- *   Computing and visualization in science 16, 4 (2013), 151-164"
- * "Vogel, A., Reiter, S., Rupp, M., Nägel, A., and Wittum, G. UG4 -- a novel
- *   flexible software system for simulating pde based models on high performance
- *   computers. Computing and visualization in science 16, 4 (2013), 165-179"
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
+ * navier_stokes_fe.h
+ *
+ *  Created on: 20.09.2010
+ *      Author: andreasvogel
  */
 
 #ifndef __H__UG__PLUGINS__NAVIER_STOKES__INCOMPRESSIBLE__FE__NAVIER_STOKES_FE__
@@ -88,6 +63,18 @@ class NavierStokesFE
 	///	returns density
 		SmartPtr<CplUserData<number, dim> > density() {return m_imDensity.user_data ();}
 
+	///	sets the bingham viscosity
+		void set_bingham_viscosity(SmartPtr<CplUserData<number, dim> > user);
+
+	///	returns bingham viscosity
+		SmartPtr<CplUserData<number, dim> > bingham_viscosity() {return m_imBinghamViscosity.user_data ();}
+
+	///	sets the yield stress
+		void set_yield_stress(SmartPtr<CplUserData<number, dim> > user);
+
+	///	returns yield stress
+		SmartPtr<CplUserData<number, dim> > yield_stress() {return m_imYieldStress.user_data ();}
+
 	///	sets the source function
 		void set_source(SmartPtr<CplUserData<MathVector<dim>, dim> > user);
 
@@ -127,6 +114,10 @@ class NavierStokesFE
 
 	///	Data import for density
 		DataImport<number, dim> m_imDensity;
+
+	/// Data import for bingham
+		DataImport<number, dim> m_imBinghamViscosity;
+		DataImport<number, dim> m_imYieldStress;
 
 	/// abbreviation for pressure
 		static const size_t _P_ = dim;
